855419051516bef04afee32899a314a3859e3b6d
Dustin McQuay <dustin-mcquay@pluralsight.com>
2019-10-16 17:15:55 -0600

    add minimal bootstrap css to main app

--- ui/src/App.js
+++ ui/src/App.js
@@ -15,9 +15,30 @@ function App() {
   }, []);
 
   return (
-    <ul>
-      {todos.map(todo => <li key={todo.id}>{todo.text} ({todo.sentiment.payload} - cacheHit: {todo.sentiment.cacheHit ? 'yes' : 'no'})</li>)}
-    </ul>
+    <div className="container">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>TODO</th>
+            <th>Sentiment</th>
+            <th>Cache Hit</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todos.map(todo => {
+            return (
+              <tr key={todo.id}>
+                <td>{todo.id}</td>
+                <td>{todo.text}</td>
+                <td>{todo.sentiment.payload}</td>
+                <td>{todo.sentiment.cacheHit ? 'yes' : 'no'}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
